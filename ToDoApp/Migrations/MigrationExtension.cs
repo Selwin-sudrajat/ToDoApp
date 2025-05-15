8f3332e0a6e0e1754aeb3994d2bbe7d7aa198212
Selwin <Selwin.sudrajat@berca.co.id>
2025-05-15 16:01:22 +0700

    Add edit data + docker

---
+++ ToDoApp/Migrations/MigrationExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoApp;
+using ToDoApp.Models;
+
+namespace ToDoApp.Extensions
+{
+    
+    public static class MigrationExtension
+    {
+        public static void ApplyMigration(this IApplicationBuilder app)
+        {
+            using IServiceScope scope = app.ApplicationServices.CreateScope();
+
+            using ToDoDbContext context = scope.ServiceProvider.GetRequiredService<ToDoDbContext>();
+            context.Database.Migrate();
+        }
+    }
+}
--- ToDoApp/Program.cs
+++ ToDoApp/Program.cs
@@ -3,6 +3,7 @@ using Serilog.Sinks.PostgreSQL;
 using Serilog;
 using System;
 using ToDoApp.Models;
+using ToDoApp.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -38,6 +39,10 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else if (app.Environment.IsDevelopment())
+{
+    app.ApplyMigration();
 }
 
 app.UseHttpsRedirection();
